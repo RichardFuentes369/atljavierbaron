e64144d1faa465e52fd7e1ae97a3bc6c07dc677d
RichardFuentes369 <javierbaron6@gmail.com>
2025-07-10 18:30:23 -0500

    ajsutes

--- angular/src/app/module/inicio/inicio.component.ts
+++ angular/src/app/module/inicio/inicio.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { BtnuploadComponent as BtnUpload} from './component/btnupload/btnupload.component';
 import { TableComponent as Table, TableComponent} from './component/table/table.component';
 import { ButtonModule } from 'primeng/button';
@@ -18,12 +18,20 @@ import { PrimedialogComponent } from '@component/globales/modal/primedialog/prim
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.scss'
 })
-export class InicioComponent {
+export class InicioComponent implements OnInit{
 
   constructor(
     private router: Router,
     private translate: TranslateService
   ) { }
+
+  async ngOnInit() {
+    let dataStorage = localStorage.getItem('data')
+    if(dataStorage){
+      const dataReal = JSON.parse(dataStorage)
+      console.log(dataReal)
+    }
+  }
 
   @ViewChild(TableComponent) tableComponent!: TableComponent;
   @ViewChild(PrimedialogComponent) primedialogComponent!: PrimedialogComponent;
